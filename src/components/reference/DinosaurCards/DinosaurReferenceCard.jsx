3aa3cbc7ccc2abcc870224ed251fbe179dab0624
hirutatsumi <tatsumi_hiruta@ex-ex.co.jp>
2023-04-07 18:26:07 +0900

    completed card-add-input component

--- src/components/reference/DinosaurCards/DinosaurReferenceCard.jsx
+++ src/components/reference/DinosaurCards/DinosaurReferenceCard.jsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { CategoryTitle } from "./DinosaurReferenceCard/CategoryTitle";
 import { CategoryDeleteButton } from "./DinosaurReferenceCard/CategoryDeleteButton";
 import { CardAddInput } from "./DinosaurReferenceCard/CardAddInput";
 import { Cards } from "./DinosaurReferenceCard/Cards";
 
 export const DinosaurReferenceCard = () => {
+  const [dinosaurList, setDinosaurList] = useState([]);
+
+  console.log(dinosaurList);
   return (
     <div className="dinosaurReferenceCard">
       <CategoryTitle />
       <CategoryDeleteButton />
-      <CardAddInput />
-      <Cards />
+      <CardAddInput setDinosaurList={setDinosaurList} />
+      <Cards dinosaurList={dinosaurList} />
     </div>
   );
 };
--- src/components/reference/DinosaurCards/DinosaurReferenceCard/CardAddInput.jsx
+++ src/components/reference/DinosaurCards/DinosaurReferenceCard/CardAddInput.jsx
@@ -1,5 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const CardAddInput = () => {
-  return <div></div>;
+export const CardAddInput = ({ setDinosaurList }) => {
+  const [inputDinosaurName, setInputDinosaurName] = useState("");
+
+  const handleChange = (event) => {
+    setInputDinosaurName(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    setDinosaurList((prev) => [...prev, { text: inputDinosaurName }]);
+
+    setInputDinosaurName("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={(event) => {
+            handleChange(event);
+          }}
+          value={inputDinosaurName}
+          placeholder="Which dinosaur?"
+        />
+      </form>
+    </div>
+  );
 };
